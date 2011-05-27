2315785243b5f7a067ab1b6f1fc310e66cd080d1
Guy Hizkiau <shizki@ssdl-14.cs.technion.ac.il>
2011-05-27 16:13:52 +0300

    created quad_table with _init_ and unit tests for it

--- src/quad_table.py
+++ src/quad_table.py
@@ -1,6 +1,50 @@
 
-'''
 class QuadTable():
-    def __init__(self, state):
+
+    def __init__(self, board, size, player):
+        '''
+        Constructor.
         
-'''
+        @param size: The board's width.
+        @param board: A matrix describing the board.
+        @param player: The player (Black/White) which the quad table describes.
+        '''
+        self.quads = {}
+        
+        for x in range(-1, size):
+            for y in range(-1, size):
+                self.quads[(x,y)] = findQuadType(x, y, board, size, player)
+
+    def getQuadType(self, x, y):
+        return self.quads[(x,y)]
+
+def findQuadType(x, y, board, size, player):
+    filled = 0
+    
+    if x >= 0 and y >= 0 and board[x][y] == player:
+        filled += 1
+    if x+1 < size and y >= 0 and board[x+1][y] == player:
+        filled += 1      
+    if x >= 0 and y+1 < size and board[x][y+1] == player:
+        filled += 1
+    if x+1 < size and y+1 < size and board[x+1][y+1] == player:
+        filled += 1
+    
+    if filled == 0:
+        return 'Q0'
+    if filled == 1:
+        return 'Q1'
+    elif filled == 3:
+        return 'Q3'
+    elif filled == 4:
+        return 'Q4'
+    elif filled == 2:
+        if x >= 0 and x+1 < size and y >= 0 and y+1 < size:
+            if board[x][y] == board[x+1][y+1] or board[x+1][y] == board[x][y+1]:
+                return 'Qd'
+            else:
+                return 'Q2'
+        else:
+            return 'Q2'
+        
+        
---
+++ tests/st_QuadTable.py
@@ -0,0 +1,57 @@
+'''
+Created on May 27, 2011
+
+@author: shizki
+'''
+import unittest
+from quad_table import findQuadType, QuadTable
+
+
+class Test(unittest.TestCase):
+
+    def setUp(self):       
+        self.board1 = ((' ', 'B'),('W', 'B'))
+        self.board2 = (('W', 'B'),('B', 'B'))
+        self.board3 = (('B', 'B'),('B', 'B'))
+        self.board4 = (('B', 'W'),('W', 'B'))
+        
+        self.board5 = (('B', 'B', ' '),('W', 'W', 'B'),(' ', 'W', 'W'))
+        self.quadTable1 = QuadTable(self.board5, 3, 'B')
+        self.quadTable2 = QuadTable(self.board5, 3, 'W')
+        
+    def tearDown(self):
+        pass
+
+
+    def testFindQuadType(self):
+        self.assertEqual('Q1', findQuadType(0, 0, self.board1, 2, 'W'))
+        self.assertEqual('Q2', findQuadType(0, 0, self.board1, 2, 'B'))
+        self.assertEqual('Q1', findQuadType(0, 0, self.board2, 2, 'W'))
+        self.assertEqual('Q3', findQuadType(0, 0, self.board2, 2, 'B'))
+        self.assertEqual('Q0', findQuadType(0, 0, self.board3, 2, 'W'))
+        self.assertEqual('Q4', findQuadType(0, 0, self.board3, 2, 'B'))
+        self.assertEqual('Q1', findQuadType(-1, -1, self.board3, 2, 'B'))
+        self.assertEqual('Q1', findQuadType(1, 1, self.board3, 2, 'B'))
+        self.assertEqual('Q2', findQuadType(0, -1, self.board3, 2, 'B'))
+        self.assertEqual('Qd', findQuadType(0, 0, self.board4, 2, 'W'))
+        self.assertEqual('Qd', findQuadType(0, 0, self.board4, 2, 'B'))
+
+
+    def testQuadBoardInit(self):
+        self.assertEqual('Q1', self.quadTable1.getQuadType(-1, -1))
+        self.assertEqual('Q0', self.quadTable1.getQuadType(1, 0))
+        self.assertEqual('Qd', self.quadTable1.getQuadType(0, 1))
+        self.assertEqual('Q1', self.quadTable1.getQuadType(1, 2))
+        self.assertEqual('Q2', self.quadTable1.getQuadType(0, 0))
+        self.assertEqual('Q2', self.quadTable1.getQuadType(-1, 0))
+        
+        self.assertEqual('Q1', self.quadTable2.getQuadType(0, -1))
+        self.assertEqual('Q2', self.quadTable2.getQuadType(0, 0))
+        self.assertEqual('Q3', self.quadTable2.getQuadType(1, 1))
+        self.assertEqual('Q2', self.quadTable2.getQuadType(2, 1))
+        self.assertEqual('Q1', self.quadTable2.getQuadType(2, 2))
+                
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
