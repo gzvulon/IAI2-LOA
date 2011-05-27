142849ca85d08599902a1f2c1f1031e3b8fb24a3
Vania <zvulon.back@gmail.com>
2011-05-27 13:06:04 +0300

    new evaluate API

--- src/s_eval_mass.py
+++ src/s_eval_mass.py
@@ -4,3 +4,19 @@ Created on May 26, 2011
 
 @author: inesmeya
 """
+from s_evaluator import Evaluator
+from loa_game import LinesOfActionState
+
+class CenterMassEvaluator(Evaluator):
+    
+    
+    
+      def evaluate(self, state, action =None, quad_table_ext=None):
+        '''
+        @type state: loa_game.LinesOfActionState
+        '''
+        # calculate center of mass
+        s = LinesOfActionState()
+        s.blacks
+
+    
--- src/s_evaluator.py
+++ src/s_evaluator.py
@@ -6,7 +6,13 @@ Created on May 26, 2011
 """
 
 class Evaluator():
-    def evaluate(self, state, action):
+    def evaluate(self, state, action =None, quad_table_ext=None):
+        '''
+        @param action: last action, from parent to this node
+        @param quad_table_ext: aditional iterative information:
+            dictionary? or struct with field for each evaluator
+        Those can be None for eample at the beggining of the game
+        '''
         raise Exception("Not Implemented")
     
     
